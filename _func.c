4aad0f8f744d875fa5682351901101b4a9f5e8a6
o714 <123072803+o714@users.noreply.github.com>
2023-04-08 11:47:47 +0300

    Update _func.c

--- _func.c
+++ _func.c
@@ -158,5 +158,5 @@ int print_hexa(va_list types, char map_to[], char buffer[],
 
 	a++;
 
-	return (write_unsgnd(0, i, buffer, flags, width, precision, size));
+	return (write_unsgnd(0, m, buffer, flags, width, precision, size));
 }
